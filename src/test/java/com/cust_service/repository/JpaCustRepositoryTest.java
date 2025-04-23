734691ec0ce200114a88431e87550fc24e956bf9
Jisu-Shin <sjszzang3053@gmail.com>
2025-04-23 15:41:48 +0900

    Test : 테스트 추가

--- src/test/java/com/cust_service/repository/JpaCustRepositoryTest.java
+++ src/test/java/com/cust_service/repository/JpaCustRepositoryTest.java
@@ -6,10 +6,13 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -35,6 +38,50 @@ class JpaCustRepositoryTest {
 
         //then
         assertEquals(list.size(), 2);
+    }
+
+    @Test
+    public void 고객저장() {
+        //given
+        String name = "고길동";
+        String phoneNumber = "01012345678";
+        CustSmsConsentType type = CustSmsConsentType.ALL_ALLOW;
+
+        custRepository.save(Cust.builder()
+                .name(name)
+                .phoneNumber(phoneNumber)
+                .smsConsentType(type)
+                .build());
+
+        //when
+        List<Cust> custList = custRepository.findAll();
+
+        //then
+        Cust cust = custList.get(0);
+        assertEquals(cust.getPhoneNumber(),phoneNumber);
+        assertEquals(cust.getSmsConsentType(),type);
+
+    }
+
+    @Test
+    @Rollback(false)
+    public void BaseTimeEntity등록() {
+        //given
+        LocalDateTime now = LocalDateTime.now();
+        custRepository.save(Cust.builder()
+                .name("홍길동")
+                .phoneNumber("01023456789")
+                .smsConsentType(CustSmsConsentType.ALL_ALLOW)
+                .build());
+
+        //when
+        Cust cust = custRepository.findAll().get(0);
+
+        //then
+        System.out.println(">>>>>>>>> createDate = "+cust.getCreatedDate()+ ", modifiedDate = " + cust.getModifiedDate());
+
+        assertThat(cust.getCreatedDate()).isAfter(now);
+        assertThat(cust.getModifiedDate()).isAfter(now);
     }
 
 }
